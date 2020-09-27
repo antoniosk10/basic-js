8cae4dd35563fb14e8b26c8599f387937f84f297
Your Name <you@example.com>
2020-09-27 17:40:30 +0300

    feat:add solution to 'dream-team'

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    let result = "";
+    if (Array.isArray(members)) {
+
+        members.forEach((el) => {
+            if (typeof el === "string") {
+                result += el.trim()[0].toUpperCase();
+            }
+        });
+
+        return result.split("").sort().join("");
+    }
+    return false;
+
 };
